8a098c11d4bfb334b96042f9fb7389fc6dfbb0cc
howsilence <statton0104@gmail.com>
2022-01-19 22:05:19 -0500

    commit tree d68c9b6048e92e3a9e5374971468a57911fa73d3

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const likeHearts = document.querySelectorAll( ".like-glyph" );
+
 
 // Your JavaScript code goes here!
+function handleIt( e ) {
+  const heart = e.target;
+  mimicServerCall( "fakeURL" )
+  .then( () => {
+    if ( heart.innerText === EMPTY_HEART){
+      heart.innerText = FULL_HEART;
+      heart.ClassName = "activated-heart";
+    } else {
+      heart.innerText = EMPTY_HEART;
+      heart.ClassName = "";
+    }
+  } )
+  .catch(error => {
+    const modal = document.querySelector( "#modal" )
+    modal.className = "";
+    modal.innerText = error;
+    setTimeout( () => modal.className = "hidden", 3000 );
+  } );
+}
 
-
-
+for (const heart of likeHearts) {
+heart.addEventListener( "click", handleIt );
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
